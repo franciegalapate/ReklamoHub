a6ab1febb785da6757481c49f1c481e04ddf2d1a
Francie Galapate <francie.gabrielle2005@gmail.com>
2025-08-31 14:20:27 +0800

    Clean files

--- hello.erl
+++
@@ -1,8 +0,0 @@
--module(hello). % module name
--export([world/0, greet/1]). % function: world, args: 0
-
-world() ->
-    io:format("Hello, world!~n").
-
-greet(Name) ->
-    io:format("Hello, ~s!~n", [Name]).
