32cea2a22b9173275ba325fa5d8bd36ee693d32b
kurisu <kurisitiyan.0611@gmail.com>
2025-09-04 06:15:01 +0800

    updated:db_config.erl - added the data base connection

--- src/db_config.erl
+++ src/db_config.erl
@@ -8,9 +8,9 @@ get_db_config() ->
     #{
         host => "localhost",
         port => 3306,
-        user => "your_username",
-        password => "your_password",
-        database => "complaints_db",
+        user => "root",
+        password => "",
+        database => "reklamohub_db",
         encoding => utf8,
         pool_size => 5
     }.
