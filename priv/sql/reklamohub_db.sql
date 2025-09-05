540ef55e2a6e15956eb67c16800f2640749acedf
kurisu <kurisitiyan.0611@gmail.com>
2025-09-06 01:26:02 +0800

    updates: - Fixed pathing - updated sql (Admin, Complaints)
    TODO
    - check Gchat to consider Resident Table

--- priv/sql/reklamohub_db.sql
+++ priv/sql/reklamohub_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 03, 2025 at 11:44 PM
+-- Generation Time: Sep 05, 2025 at 05:25 PM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -29,16 +29,31 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
-  `username` varchar(50) NOT NULL,
-  `Password` varchar(50) NOT NULL
+  `admin_id` int NOT NULL AUTO_INCREMENT,
+  `admin_username` varchar(50) NOT NULL,
+  `admin_password` varchar(255) NOT NULL,
+  PRIMARY KEY (`admin_id`),
+  UNIQUE KEY `admin_username` (`admin_username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `admin`
+-- Table structure for table `complaints`
 --
 
-INSERT INTO `admin` (`username`, `Password`) VALUES
-('kurisu', 'husky');
+DROP TABLE IF EXISTS `complaints`;
+CREATE TABLE IF NOT EXISTS `complaints` (
+  `complaint_id` int NOT NULL AUTO_INCREMENT,
+  `resident` varchar(100) DEFAULT NULL,
+  `category` varchar(50) NOT NULL,
+  `status` enum('submitted','in progress','resolved','rejected') NOT NULL DEFAULT 'submitted',
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `img` varchar(255) DEFAULT NULL,
+  `address` varchar(255) NOT NULL,
+  `details` text NOT NULL,
+  PRIMARY KEY (`complaint_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
